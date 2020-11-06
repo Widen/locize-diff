59d7c9dfde1b33d8285243d8e1382b1920fba24e
Mark Skelton <mskelton@widen.com>
2020-11-06 09:25:09 -0600

    Add message when diffs haven't been run

--- __tests__/commands/copy.spec.js
+++ __tests__/commands/copy.spec.js
@@ -2,17 +2,26 @@ jest.mock('@actions/core')
 jest.mock('@actions/github')
 jest.mock('@actions/http-client')
 
-import { setFailed } from '@actions/core'
 import { contextMock, createCommentMock, prComment } from '@actions/github'
+import { putMock } from '@actions/http-client'
 import { runAction } from '../../src/runAction'
+import { mockFetchResource, mockListResources } from '../utils'
 
 beforeEach(() => {
   jest.resetAllMocks()
   contextMock.mockReturnValue(prComment('@locize-diff copy'))
+  mockListResources((version) => [`projectId/${version}/en-US/translation`])
 })
 
-it('should throw a not implemented error', async () => {
+it("should display the diff without copying if the user hasn't run the diff yet", async () => {
+  mockFetchResource(
+    { 'en-US/translation': { foo: 'bar' } },
+    { 'en-US/translation': { foo: 'baz' } }
+  )
+
   await runAction()
-  expect(setFailed).toHaveBeenCalledWith('Not implemented yet')
-  expect(createCommentMock).not.toHaveBeenCalled()
+  expect(createCommentMock).toHaveBeenCalledTimes(1)
+  expect(createCommentMock.mock.calls[0][0].issue_number).toBe(123)
+  expect(createCommentMock.mock.calls[0][0].body).toMatchSnapshot()
+  expect(putMock).not.toHaveBeenCalled()
 })
--- src/commands/copy.ts
+++ src/commands/copy.ts
@@ -1,3 +1,18 @@
-export function runCopy() {
-  throw new Error('Not implemented yet')
+import { context } from '@actions/github'
+import { getComment } from '../utils/comments'
+import { getDiffs } from '../utils/diff'
+import { createMessage } from '../utils/message'
+
+export async function runCopy() {
+  const diffs = await getDiffs()
+  const comment = await getComment()
+
+  if (!comment && diffs.length) {
+    const issueType = context.payload.pull_request ? 'pull request' : 'issue'
+
+    return createMessage(
+      diffs,
+      `It looks like you haven't checked for diffs in this ${issueType} yet. Please check the diffs in this comment and then run \`@locize-diff copy\` again.`
+    )
+  }
 }
